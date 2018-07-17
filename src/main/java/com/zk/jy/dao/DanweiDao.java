f2d98b62306b278d8e817e0d366367d7b96c2c17
Wu <wdw_shen@hotmail.com>
2018-07-17 11:45:42 +0800

    遍历单位信息dao和servlet测试成功

--- src/main/java/com/zk/jy/dao/DanweiDao.java
+++ src/main/java/com/zk/jy/dao/DanweiDao.java
@@ -1,12 +1,18 @@
 package com.zk.jy.dao;
 
+import com.zk.jy.utils.ZkDB;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class DanweiDao {
 
     public static ResultSet allDanwei(Connection conn) throws Exception{
-        String sql = "select * from t_anwei order by dlevel";
-        return null;
+        String sql = "select * from t_danwei_info order by dlevel";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+        return rs;
     }
+
 }
--- src/main/java/com/zk/jy/servlets/DanweiSearchServlet.java
+++ src/main/java/com/zk/jy/servlets/DanweiSearchServlet.java
@@ -1,10 +1,19 @@
 package com.zk.jy.servlets;
 
+import com.sun.org.apache.regexp.internal.RE;
+import com.zk.jy.dao.DanweiDao;
+import com.zk.jy.utils.JsonUtil;
+import com.zk.jy.utils.ResponseUtil;
+import com.zk.jy.utils.ZkDB;
+import org.json.JSONArray;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 public class DanweiSearchServlet extends HttpServlet {
     @Override
@@ -17,5 +26,13 @@ public class DanweiSearchServlet extends HttpServlet {
 
         //获取任务action，任务包括：显示所有单位（showall）、通过名称查找单位（findbyname）、通过地址查找单位（findbyaddress）
         String action = request.getParameter("action");
+        try {
+            Connection conn = ZkDB.getConn();
+            ResultSet rs = DanweiDao.allDanwei(conn);
+            JSONArray result = JsonUtil.formatRsToJsonArray(rs);
+            ResponseUtil.write(response,result);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
